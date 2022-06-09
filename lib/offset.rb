9ffd219b902a45f1b2589f829c13c1a16dc52d74
Jonathan Pope <jonathanmpope@gmail.com>
2022-06-09 17:15:05 -0600

    feat: date gen method for offset class

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,7 +1,17 @@
 class Offset
+  attr_reader :date 
 
-  def initialize
-    # @argument = argument
+  def initialize(date = date_gen)
+    @date = date
   end
+
+  def date_gen
+    today = Time.now.to_s
+    y = today[2..3]
+    m = today[5..6]
+    d = today[8..9]
+    @date = m.concat(d).concat(y)
+  end
+
 
 end
--- spec/offset_spec.rb
+++ spec/offset_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe do
     it 'exists' do
       expect(@offset).to be_instance_of Offset
     end
+
+    it 'sets the date as today if none is provided' do
+      date = Time.now.to_s
+      y = date[2..3]
+      m = date[5..6]
+      d = date[8..9]
+      final = m.concat(d).concat(y)
+      expect(@offset.date).to eq(final)
+    end
   end
