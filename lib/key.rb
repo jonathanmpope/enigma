f73e4ef5bbd185fe20bb77daf2d8f307351b3394
Jonathan Pope <jonathanmpope@gmail.com>
2022-06-09 16:44:17 -0600

    feat: add a key method

--- lib/key.rb
+++ lib/key.rb
@@ -1,15 +1,19 @@
 class Key
-  attr_reader :num
+  attr_reader :num, :a_key
 
   def initialize(num = num_gen)
     @num = num
-    # @argument = argument
+    @a_key = a_key_gen
   end
 
   def num_gen
     num = rand(0..99999)
     @num = "%05d" % num
+  end
 
+  def a_key_gen
+    @a_key = @num[0..1]
+    # binding.pry
   end
 
 end
--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 RSpec.describe do
   before :each do
       @key = Key.new
+      @key_2 = Key.new("015523")
     end
 
     it 'exists' do
@@ -12,6 +13,13 @@ RSpec.describe do
     it 'creates a random five digit number' do
       expect(@key.num.length).to eq(5)
       expect(@key.num.class).to eq(String)
+      expect(@key_2.num).to eq("015523")
+    end
+
+    it 'creates an A key' do
+      expect(@key.a_key.length).to eq(2)
+      expect(@key.a_key.class).to eq(String)
+      expect(@key_2.a_key).to eq("01")
     end
 
 end
