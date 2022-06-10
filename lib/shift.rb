7fb764757dd261c65b499c0dd8eea03c4cfd880d
Jonathan Pope <jonathanmpope@gmail.com>
2022-06-10 08:05:12 -0600

    feat: create shift method

--- lib/shift.rb
+++ lib/shift.rb
@@ -4,4 +4,12 @@ class Shift
     # @argument = argument
   end
 
+  def create_shift(key, offset)
+    @a_shift = key.a_key.to_i + offset.a_key.to_i
+    @b_shift = key.b_key.to_i + offset.b_key.to_i
+    @c_shift = key.c_key.to_i + offset.c_key.to_i
+    @d_shift = key.d_key.to_i + offset.d_key.to_i
+    "#{@a_shift}#{@b_shift}#{@c_shift}#{@d_shift}"
+  end
+
 end
--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -3,11 +3,15 @@ require 'spec_helper'
 RSpec.describe do
   before :each do
     @shift = Shift.new
-    @offset = Offset.new
-    @key = Key.new
+    @offset = Offset.new("011092")
+    @key = Key.new("01552")
   end
 
     it 'exists' do
       expect(@shift).to be_instance_of Shift
     end
-end 
+
+    it 'creates the shifts' do
+      expect(@shift.create_shift(@key, @offset)).to eq("3196156")
+    end
+end
